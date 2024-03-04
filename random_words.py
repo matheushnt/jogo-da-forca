8ce184bed8c42a1c70261d7aa39ed3a4f1098d0f
Matheus <matheushnt06@gmail.com>
2024-03-04 15:12:07 -0300

    Nomes de animais a serem sorteados como uma palavra secreta

---
+++ random_words.py
@@ -0,0 +1,21 @@
+palavras = [
+    'leão',
+    'borboleta',
+    'elefante',
+    'pinguim',
+    'girafa',
+    'cachorro',
+    'gato',
+    'tubarão',
+    'arara',
+    'tartaruga',
+    'orangotango',
+    'aguia',
+    'coala',
+    'rinoceronte',
+    'polvo',
+    'sapo',
+    'pantera',
+    'camaleão',
+    'canguru'
+]
